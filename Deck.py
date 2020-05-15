5342315979c77657091033cdebccd9e3aef578e0
KlassCode <klassenedmond7@gmail.com>
2020-05-15 00:50:19 -0400

    Adding class card and Deck

---
+++ Deck.py
@@ -0,0 +1,15 @@
+import random
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+
+        for i in range(2,15):
+            for j in range(4):
+                self.cards.append(Card(i,j))
+        random.shuffle(self.cards)
+
+    def rm_card(self):
+        if len(self.cards) == 0:
+            return
+        return self.cards.pop()
